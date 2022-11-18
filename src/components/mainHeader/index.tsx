d5d7c61d958e29ceb0223b68951eac7f075716df
johnatan sousa paixao <johnatan.sousa@gmail.com>
2022-11-17 23:38:40 -0300

    styled header

--- src/components/mainHeader/index.tsx
+++ src/components/mainHeader/index.tsx
@@ -1,9 +1,16 @@
-import { Container } from './styles'
+import { Container, Profile, Welcome, UserName } from './styles'
 
 export default function MainHeader() {
     return (
         <Container>
             <h1>main Header</h1>
+
+            <Profile>
+                <Welcome>Óla </Welcome>
+                <UserName>
+                    johnatan Paixão
+                </UserName>
+            </Profile>
         </Container>
     )
 }
--- src/components/mainHeader/styles.ts
+++ src/components/mainHeader/styles.ts
@@ -4,4 +4,22 @@ export const Container = styles.div`
     grid-area: MH;
     color: ${props => props.theme.colors.white};
     background-color: ${props => props.theme.colors.secondary};
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 10px;
+    border-bottom: 1px solid ${props => props.theme.colors.white}
 `;
+
+export const Welcome = styles.h3`
+
+`;
+
+export const Profile = styles.div`
+    border-bottom: 1px solid ${props => props.theme.colors.white}
+`;
+
+export const UserName = styles.span`
+
+`;
+
