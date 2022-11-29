045df48cc35df0baf0700e6fbcf578cbbf367410
johnatan sousa paixao <johnatan.sousa@gmail.com>
2022-11-29 00:14:19 -0300

    select

--- src/components/contentHeader/index.tsx
+++ src/components/contentHeader/index.tsx
@@ -1,9 +1,23 @@
-import { Container } from './styles'
+import { Container, Title, Controllers } from './styles'
+import SelectInput from "../../components/selectInput";
 
 export default function ContentHeader() {
+
+    const options = [
+        {value:"johnatan", label:"Johnatan"},
+        {value:"joe", label:"joe"},
+        {value:"cloe", label:"cloe"},
+    ]
+
     return (
         <Container>
-            <h1>ContentHeader</h1>
+            <Title>
+                <h1>ContentHeader</h1>
+            </Title>
+            <Controllers>
+                <SelectInput options={options}/>
+                <SelectInput options={options}/>
+            </Controllers>
         </Container>
     )
 }
