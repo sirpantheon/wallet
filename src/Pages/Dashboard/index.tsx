dcaf2c4a5fefcf3fd0dc88872c1e8083c896a2e1
johnatan sousa paixao <johnatan.sousa@gmail.com>
2022-12-17 23:10:37 -0300

    dashboard refatoração

---
+++ src/Pages/Dashboard/index.tsx
@@ -0,0 +1,22 @@
+
+import { SelectInput } from "../../Components/SelectInput";
+import { ContentHeader } from "../../Components/contentHeader";
+import { Container } from "./styles";
+
+
+export default function Dashboard() {
+
+    const options = [
+        {value: "2022", label: "2022"},
+        {value: "2023", label: "2023"},
+        {value: "2024", label: "2024"}
+    ]
+
+    return (
+        <Container>
+            <ContentHeader title="DashBoard" lineColor="#E44C4E">
+                <SelectInput options={options}/>
+            </ContentHeader>
+        </Container>
+    )
+}
