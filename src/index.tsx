191f303308e06362bdd84e43e8c712979b67306b
johnatan sousa paixao <johnatan.sousa@gmail.com>
2023-01-08 00:20:02 -0300

    fg

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import GlobalStyle from './Styles/GlobalStyles';
 import { ThemeProvider } from 'styled-components';
-import { dark, light } from './Styles/themes/themes';
+import { dark } from './Styles/themes/themes';
 import Routes from './Routes';
 
 const root = ReactDOM.createRoot(
