415f7350b08c0a0f0aaef566b29c0615a679f629
johnatan sousa paixao <johnatan.sousa@gmail.com>
2023-01-08 00:17:42 -0300

    Revert

--- src/Pages/Dashboard/index.tsx
+++ src/Pages/Dashboard/index.tsx
@@ -1,5 +1,5 @@
 
-import { useMemo, useState, useCallback } from "react";
+import { useMemo, useState } from "react";
 import { SelectInput } from "../../Components/SelectInput";
 import { ContentHeader } from "../../Components/contentHeader";
 import { Container, Content } from "./styles";
@@ -17,16 +17,16 @@ export default function Dashboard() {
     const [monthSelected, setMonthSelected] = useState<number>(new Date().getMonth() + 1);
     const [yearSelected, setYearSelected] = useState<number>(new Date().getFullYear());
 
-    const handleMonthSelected = useCallback((month: string) => {
+    function handleMonthSelected(month: string) {
         try {
             const parseMonth = Number(month)
             setMonthSelected(parseMonth)
         } catch {
             throw new Error('Més Invalido')
         }
-    },[])
+    }
 
-    const handleYearSelected = useCallback((year: string)=> {
+    function handleYearSelected(year: string) {
         try {
             const parseYear = Number(year)
             setYearSelected(parseYear)
@@ -34,7 +34,7 @@ export default function Dashboard() {
             throw new Error('Ano Invalido')
 
         }
-    },[])
+    }
 
     const years = useMemo(() => {
         let uniqueYears: number[] = [];
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import GlobalStyle from './Styles/GlobalStyles';
 import { ThemeProvider } from 'styled-components';
-import dark from './Styles/themes/dark';
+import { dark, light } from './Styles/themes/themes';
 import Routes from './Routes';
 
 const root = ReactDOM.createRoot(
